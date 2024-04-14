0e6556a1d0d795957dfc12f05262084c171c1ecf
dheeraj singh <dheerajsingh@dheerajs-MacBook-Pro.local>
2024-04-14 14:33:16 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,56 @@
 
 import './App.css';
 import Login from './components/Login';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import HomePage from './components/HomePage';
 import PendingTransactions from './components/PendingTransactions';
 import AdminPage from './components/AdminPage';
 import GetLatestGameDetails from './components/GetLatestGameDetails';
+import { useState } from 'react';
 
 function App() {
+
+  const [x,setx] = useState(0);
+
+  
+
+
   return (
-    
-    <BrowserRouter>
+    <>
+    <div>
+      <button onClick={()=>setx(0)}>HomePage</button>
+      <button onClick={()=>setx(1)}>Login</button>
+      <button onClick={()=>setx(2)}>Register</button>
+      <button onClick={()=>setx(3)}>PendingTransactions</button>
+      <button onClick={()=>setx(4)}>GetLatestGameDetails</button>
+      <button onClick={()=>setx(5)}>AdminPage</button>
+    </div>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/pendingTransactions" element={<PendingTransactions/>}/>
-        <Route path="/latestGameDetails" element={<GetLatestGameDetails/>}/>
-        <Route path='/admin' element={<AdminPage/>}/>
+        {x===0 &&
+        <Route path="/" Component={HomePage}/>}
+        ||
+        {x===1 &&
+        <Route path="/" Component={Login}/>}
+        ||
+        {x===2 &&
+        <Route path="/" Component={Register}/>}
+        ||
+        {x===3 && 
+        <Route path="/" Component={PendingTransactions}/>}
+        ||
+        {x===4 && 
+        <Route path="/" Component={GetLatestGameDetails}/>}
+        ||
+        {x==5 &&
+        <Route path='/' Component={AdminPage}/>}
         {/* <Route path="*" element={"<NoPageFound />"} /> */}
 
       </Routes>
-      
-    </BrowserRouter>
+
+    </HashRouter>
+    </>
   );
 }
 
